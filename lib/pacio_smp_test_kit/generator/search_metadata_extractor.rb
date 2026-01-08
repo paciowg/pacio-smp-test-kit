61a308e9e3a3c58f12868088ddda7bb33531ea1f
Yunwei Wang <>
2026-01-08 08:24:05 -0600

    commented out additional search parameters in search_metadata_extractor

--- lib/pacio_smp_test_kit/generator/search_metadata_extractor.rb
+++ lib/pacio_smp_test_kit/generator/search_metadata_extractor.rb
@@ -8,17 +8,17 @@ module PacioSMPTestKit
         result = super
         
         case resource_capabilities.type
-        when 'Patient'
-          if result.none? { |p| p[:names] == ['_id'] }
-            result << { names: ['_id'], expectation: 'SHALL' } 
-          end
+        # when 'Patient'
+        #   if result.none? { |p| p[:names] == ['_id'] }
+        #     result << { names: ['_id'], expectation: 'SHALL' } 
+        #   end
         when 'List'
           result.clear
           result << { names: ['patient', 'code'], expectation: 'SHALL' }     
-        when 'MedicationAdministration', 'MedicationRequest', 'MedicationStatement'
-          if result.none? { |p| p[:names] == ['patient'] }
-            result << { names: ['patient'], expectation: 'SHALL' }
-          end
+        # when 'MedicationAdministration', 'MedicationRequest', 'MedicationStatement'
+        #   if result.none? { |p| p[:names] == ['patient'] }
+        #     result << { names: ['patient'], expectation: 'SHALL' }
+        #   end
         end
         
         result
